cc55aa3cc5f2e5ae303aedf8cd36ffce8333e024
kenkatou354 <kenkatou354@gmail.com>
2026-01-01 04:53:49 +0900

    [clear]不要なusingと空白を削除

--- Assets/InventorySample/Inventory.cs
+++ Assets/InventorySample/Inventory.cs
@@ -1,20 +1,17 @@
-﻿using NUnit.Framework;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace InventorySample
 {
     public class Inventory
     {
-
-
-
         /// <summary>
         /// アイテムを所持リストに追加する
         /// すてに所持してる場所合は数量を加算する
         /// </summary>
-        public void AddItem(Item item,int amount)
+        public void AddItem(ItemData item, int amount)
         {
+            if (amount == 0) return;
             // すでに同じアイテムを所持しているか確認する
             if (_itemsDic.ContainsKey(item))
             {
@@ -30,13 +27,21 @@ namespace InventorySample
         /// <summary>
         /// 現在の所持リストを取得する
         /// </summary>
-        public Dictionary<Item,int> GetItemList()
+        public Dictionary<ItemData, int> GetItemList()
         {
-           return _itemsDic;
+            return _itemsDic;
+        }
+
+        public void PrintItemList()
+        {
+            foreach (var kvp in _itemsDic)
+            {
+                Debug.Log($"Item: {kvp.Key.ItemName}, Amount: {kvp.Value}");
+            }
         }
         /// <summary>
         /// 所持アイテムと数量を管理する辞書
         /// </summary>
-        private Dictionary<Item,int> _itemsDic = new Dictionary<Item,int>();
+        private Dictionary<ItemData, int> _itemsDic = new Dictionary<ItemData, int>();
     }
 }
