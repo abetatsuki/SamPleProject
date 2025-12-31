bbf00f3ef0f706acd0be32a41cc4e108c966f881
kenkatou354 <kenkatou354@gmail.com>
2026-01-01 06:00:55 +0900

    [add]インベントリの所持アイテムを購読して表示する処理を追加

--- Assets/InventorySample/Develop/InventorySample.cs
+++ Assets/InventorySample/Develop/InventorySample.cs
@@ -1,10 +1,11 @@
-﻿using UnityEngine;
-using UniRx;
+﻿using UniRx;
+using UnityEngine;
 namespace InventorySample.Develop
 {
     public class InventorySample : MonoBehaviour
     {
         private UniRxItemModel _uniRxModel;
+        private UniRxInventory _uniRxInventory;
         private Inventory _inventory;
         [SerializeField]
         private ItemData[] _itemData;
@@ -13,7 +14,9 @@ namespace InventorySample.Develop
         {
             _inventory = new Inventory();
             _uniRxModel = new UniRxItemModel();
+            _uniRxInventory = new UniRxInventory();
             PrintModelCount(_uniRxModel);
+            PrintInventory(_uniRxInventory);
         }
 
         private void Update()
@@ -22,14 +25,16 @@ namespace InventorySample.Develop
             if (_timer >= 2f)
             {
                 _timer = 0f;
-                _uniRxModel.AddAmount(1);
+                //  _uniRxModel.AddAmount(1);
+                _uniRxInventory.AddItem(_itemData[Random.Range(0, _itemData.Length)], 1);
+                _uniRxInventory.RemoveItem(_itemData[Random.Range(0, _itemData.Length)], 1);
             }
 
         }
 
         private void InventoryTest()
         {
-         
+
             foreach (var itemData in _itemData)
             {
                 // 各アイテムを5個ずつ追加する
@@ -53,9 +58,37 @@ namespace InventorySample.Develop
                 .AddTo(this);
         }
 
+        /// <summary>
+        /// インベントリの所持アイテムを購読して表示する
+        /// </summary>
+        private void PrintInventory(UniRxInventory inventory)
+        {
+            inventory.ItemsDic
+                .ObserveAdd()
+                .Subscribe(addEvent =>
+                {
+                    Debug.Log($"Added Item: {addEvent.Key.ItemName}, Amount: {addEvent.Value}");
+                })
+                .AddTo(this);
+            inventory.ItemsDic
+                .ObserveReplace()
+                .Subscribe(replaceEvent =>
+                {
+                    Debug.Log($"Updated Item: {replaceEvent.Key.ItemName}, New Amount: {replaceEvent.NewValue}");
+                })
+                .AddTo(this);
+            inventory.ItemsDic
+                .ObserveRemove()
+                .Subscribe(x =>
+                {
+                    // 削除されたアイテムを表示する
+                    Debug.Log($"Removed: {x.Key.name}");
+                })
+                .AddTo(this);
+
+        }
 
 
-        
 
     }
 }
