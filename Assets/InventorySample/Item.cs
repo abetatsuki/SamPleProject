9613387eb95de29b2a82634d4f52df2fa53822ee
kenkatou354 <kenkatou354@gmail.com>
2026-01-01 04:35:31 +0900

    [add]アイテムクラスにコンストラクタを追加

--- Assets/InventorySample/Item.cs
+++ Assets/InventorySample/Item.cs
@@ -4,8 +4,15 @@ namespace InventorySample
 {
     public class Item
     {
+        /// <summary>
+        /// アイテム生成時のコンストラクタ
+        /// </summary>    
+        public Item(string itemName)
+        {
+            this.itemName = itemName;
+        }
         public string ItemName => itemName;
-        [SerializeField]
+
         private string itemName;
 
     }
