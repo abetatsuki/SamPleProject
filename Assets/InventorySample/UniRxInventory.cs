eb013dd609a4fc1ba1f14987694dffd338c832d8
kenkatou354 <kenkatou354@gmail.com>
2026-01-01 05:40:17 +0900

    [add]UniRxで管理するインベントリを追加

--- Assets/InventorySample/UniRxInventory.cs
+++ Assets/InventorySample/UniRxInventory.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
-
+using UniRx;
 namespace InventorySample
 {
   public class UniRxInventory : MonoBehaviour
   {
 
+        ReactiveProperty<int> _itemCount = new ReactiveProperty<int>();
   }
 }
