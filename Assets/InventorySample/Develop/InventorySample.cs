2e0fad1d0e9996a919a658c1597b4997dce6e6e3
kenkatou354 <kenkatou354@gmail.com>
2026-01-01 05:39:56 +0900

    [add] 時間でアイテムを増やす、ModelのReactivPropertyに登録する処理を追加

--- Assets/InventorySample/Develop/InventorySample.cs
+++ Assets/InventorySample/Develop/InventorySample.cs
@@ -1,12 +1,35 @@
 ﻿using UnityEngine;
-
+using UniRx;
 namespace InventorySample.Develop
 {
-  public class InventorySample : MonoBehaviour
-  {
+    public class InventorySample : MonoBehaviour
+    {
+        private UniRxItemModel _uniRxModel;
+        private Inventory _inventory;
+        [SerializeField]
+        private ItemData[] _itemData;
+        private float _timer = 0f;
         private void Start()
         {
             _inventory = new Inventory();
+            _uniRxModel = new UniRxItemModel();
+            PrintModelCount(_uniRxModel);
+        }
+
+        private void Update()
+        {
+            _timer += Time.deltaTime;
+            if (_timer >= 2f)
+            {
+                _timer = 0f;
+                _uniRxModel.AddAmount(1);
+            }
+
+        }
+
+        private void InventoryTest()
+        {
+         
             foreach (var itemData in _itemData)
             {
                 // 各アイテムを5個ずつ追加する
@@ -16,9 +39,23 @@ namespace InventorySample.Develop
             _inventory.PrintItemListItemId();
         }
 
-        private Inventory _inventory;
-        [SerializeField]
-        private ItemData[] _itemData;
+        /// <summary>
+        /// モデルの Amount を購読して表示する
+        /// </summary>
+        private void PrintModelCount(UniRxItemModel model)
+        {
+            model.Amount
+                .Subscribe(amount =>
+                {
+                    // Amount が更新されたときの処理
+                    Debug.Log($"Current Amount: {amount}");
+                })
+                .AddTo(this);
+        }
+
+
+
+        
 
     }
 }
