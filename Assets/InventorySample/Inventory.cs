43aef6ba2caf5fa13ac6692cf8fc538cbf6fb738
kenkatou354 <kenkatou354@gmail.com>
2026-01-01 04:28:48 +0900

    [add]所持リストと所持リストに追加する処理を追加

---
+++ Assets/InventorySample/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InventorySample
+{
+    public class Inventory
+    {
+
+
+
+        /// <summary>
+        /// アイテムを所持リストに追加する
+        /// すてに所持してる場所合は数量を加算する
+        /// </summary>
+        public void AddItem(Item item,int amount)
+        {
+            // すでに同じアイテムを所持しているか確認する
+            if (_itemsDic.ContainsKey(item))
+            {
+                // 既存の数量に加算する
+                _itemsDic[item] += amount;
+            }
+            else
+            {
+                // 新しいアイテムとして追加する
+                _itemsDic.Add(item, amount);
+            }
+        }
+        /// <summary>
+        /// 所持アイテムと数量を管理する辞書
+        /// </summary>
+        private Dictionary<Item,int> _itemsDic = new Dictionary<Item,int>();
+    }
+}
