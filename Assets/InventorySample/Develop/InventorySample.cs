477c222ffee1546034963941f47827d3d5927efc
kenkatou354 <kenkatou354@gmail.com>
2026-01-01 04:53:32 +0900

    [add]インベントリテスト用スクリプトを追加

---
+++ Assets/InventorySample/Develop/InventorySample.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace InventorySample.Develop
+{
+  public class InventorySample : MonoBehaviour
+  {
+        private void Start()
+        {
+            _inventory = new Inventory();
+            foreach (var itemData in _itemData)
+            {
+                // 各アイテムを5個ずつ追加する
+                _inventory.AddItem(itemData, 5);
+            }
+            _inventory.PrintItemList();
+        }
+
+        private Inventory _inventory;
+        [SerializeField]
+        private ItemData[] _itemData;
+
+    }
+}
