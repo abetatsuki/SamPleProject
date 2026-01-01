a25c72854ae7803337d03cfe55f11ac19f0be97f
kenkatou354 <kenkatou354@gmail.com>
2026-01-01 21:36:56 +0900

    [add]プレイヤーの入力を受け取るクラス追加

---
+++ Assets/InventorySample/PlayerInputSystem.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UniRx;
+using UnityEngine.InputSystem;
+
+namespace InventorySample
+{
+    public class PlayerInputSystem
+    {
+        public PlayerInputSystem(PlayerInput playerInput)
+        {
+            _playerInput = playerInput;
+            _selectAction = _playerInput.actions[SelectActionName];
+            _selectAction.started += Select;
+
+        }
+
+
+        public IReadOnlyReactiveProperty<int> SelectIndex => _selectIndex;
+
+        public void Select(InputAction.CallbackContext ctx)
+        {
+            float value = ctx.ReadValue<float>();
+            int delta = Mathf.RoundToInt(value);
+            _selectIndex.Value += delta;
+            Debug.Log(_selectIndex.Value);
+        }
+
+
+
+        private readonly ReactiveProperty<int> _selectIndex = new ReactiveProperty<int>(0);
+        private PlayerInput _playerInput;
+        private InputAction _selectAction;
+        private const string SelectActionName = "Select";
+
+    }
+}
