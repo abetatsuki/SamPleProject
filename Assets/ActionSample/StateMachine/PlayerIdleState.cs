cdb82bdb8b5ddc409e3bcdf666f53ee7c86e5496
kenkatou354 <kenkatou354@gmail.com>
2026-01-03 18:23:02 +0900

    [add] Idleステートを追加

---
+++ Assets/ActionSample/StateMachine/PlayerIdleState.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace ActionSample.StateMachine
+{
+    public class PlayerIdleState : PlayerState
+    {
+        public PlayerIdleState(PlayerController context) : base(context) { }
+
+        public override void Enter()
+        {
+            // 停止させる
+            Vector3 currentVel = ctx.Rigidbody.linearVelocity;
+            ctx.Rigidbody.linearVelocity = new Vector3(0, currentVel.y, 0);
+        }
+
+        public override void LogicUpdate()
+        {
+            base.LogicUpdate();
+
+            // 移動入力があればWalkへ
+            if (ctx.InputHandler.MovementInput.sqrMagnitude > 0.01f)
+            {
+                ctx.StateMachine.ChangeState(ctx.WalkState);
+            }
+        }
+    }
+}
