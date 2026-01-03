b17aeb6e66f9ce42ab3379f5dee1bd370b7c8b52
kenkatou354 <kenkatou354@gmail.com>
2026-01-03 18:24:16 +0900

    [add]ステートマシーンの初期化処理を追加

--- Assets/ActionSample/PlayerController.cs
+++ Assets/ActionSample/PlayerController.cs
@@ -1,50 +1,63 @@
 using UnityEngine;
+using ActionSample.StateMachine;
 
 namespace ActionSample
 {
-    // 依存関係を明示
     [RequireComponent(typeof(PlayerInputHandler))]
-    [RequireComponent(typeof(PlayerMovement))]
+    [RequireComponent(typeof(Rigidbody))]
     [RequireComponent(typeof(PlayerAiming))]
     public class PlayerController : MonoBehaviour
     {
-        private PlayerInputHandler _input;
-        private PlayerMovement _movement;
-        private PlayerAiming _aiming;
+        [Header("Movement Settings")]
+        public float MoveSpeed = 5f;
+
+        [Header("Slide Settings")]
+        public float SlideSpeed = 15f;
+        public float SlideDuration = 0.8f;
+        public float SlideDrag = 0.5f;
+
+        // 公開プロパティ (Dependency Injection for States)
+        public Rigidbody Rigidbody { get; private set; }
+        public PlayerInputHandler InputHandler { get; private set; }
+        public PlayerAiming Aiming { get; private set; }
+        public StateMachine.StateMachine StateMachine { get; private set; }
+
+        // ステートのインスタンス
+        public PlayerIdleState IdleState { get; private set; }
+        public PlayerWalkState WalkState { get; private set; }
+        public PlayerSlideState SlideState { get; private set; }
 
         private void Awake()
         {
-            // コンポーネントの取得
-            _input = GetComponent<PlayerInputHandler>();
-            _movement = GetComponent<PlayerMovement>();
-            _aiming = GetComponent<PlayerAiming>();
+            Rigidbody = GetComponent<Rigidbody>();
+            InputHandler = GetComponent<PlayerInputHandler>();
+            Aiming = GetComponent<PlayerAiming>();
+
+            // ステートマシンの初期化
+            StateMachine = new StateMachine.StateMachine();
+            IdleState = new PlayerIdleState(this);
+            WalkState = new PlayerWalkState(this);
+            SlideState = new PlayerSlideState(this);
+        }
+
+        private void Start()
+        {
+            StateMachine.Initialize(IdleState);
         }
 
         private void Update()
         {
-            // 入力に基づいてロジックを実行
+            // 現在のステートのロジック更新
+            StateMachine.CurrentState.LogicUpdate();
 
-            // スライディング入力のチェック (移動入力がある時のみ)
-            if (_input.SlideTriggered && _input.MovementInput.magnitude > 0.1f)
-            {
-                _movement.StartSlide(_input.MovementInput);
-            }
-
-            // 照準の更新
-            _aiming.UpdateAiming(_input.MousePosition);
+            // 照準はステートに関わらず更新（必要ならステート内に移動も可）
+            Aiming.UpdateAiming(InputHandler.MousePosition);
         }
 
         private void FixedUpdate()
         {
-            // 物理挙動の更新
-            if (_movement.IsSliding)
-            {
-                _movement.HandleSlideUpdate();
-            }
-            else
-            {
-                _movement.Move(_input.MovementInput);
-            }
+            // 現在のステートの物理更新
+            StateMachine.CurrentState.PhysicsUpdate();
         }
     }
 }
