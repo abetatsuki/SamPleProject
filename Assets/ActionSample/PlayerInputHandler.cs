a11da6742f40faf7f3c7b05e4c8ca14b55a73bf8
kenkatou354 <kenkatou354@gmail.com>
2026-01-05 03:56:22 +0900

    [add]銃アクションプロパティを追加

--- Assets/ActionSample/PlayerInputHandler.cs
+++ Assets/ActionSample/PlayerInputHandler.cs
@@ -1,12 +1,15 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace ActionSample
 {
     public class PlayerInputHandler : MonoBehaviour
     {
         public Vector3 MovementInput { get; private set; }
+        public bool AimInput { get; private set; }
         public bool SlideTriggered { get; private set; }
         public Vector3 MousePosition { get; private set; }
+        public bool ReloadInput {get; private set; }
+        public bool FireInput { get; private set; }
 
         private void Update()
         {
@@ -14,7 +17,9 @@ namespace ActionSample
             float x = Input.GetAxisRaw("Horizontal");
             float z = Input.GetAxisRaw("Vertical");
             MovementInput = new Vector3(x, 0, z).normalized;
-
+            AimInput = Input.GetMouseButton(1);
+            ReloadInput = Input.GetKeyDown(KeyCode.R);
+            FireInput = Input.GetMouseButton(0);
             SlideTriggered = Input.GetKeyDown(KeyCode.LeftControl);
             MousePosition = Input.mousePosition;
         }
