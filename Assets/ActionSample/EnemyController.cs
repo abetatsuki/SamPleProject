81ccd2c0c4201d0d131226b878f9251e6e932e14
kenkatou354 <kenkatou354@gmail.com>
2026-01-04 22:44:35 +0900

    [add]スライド状態で敵が接触したときの処理を追加

---
+++ Assets/ActionSample/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace ActionSample
+{
+    public class EnemyController : MonoBehaviour
+    {
+
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (!collision.gameObject.CompareTag("Player")) return;
+            Debug.Log("Enemy collided with Player!");
+            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+            if (player.StateMachine.CurrentState == player.IdleState)
+            {
+                Debug.Log("Player is in Idle State. Enemy takes action!");
+            }
+        }
+    }
+}
