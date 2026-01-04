2862723c6e7c9933147dee84f3a41248ecad3d97
kenkatou354 <kenkatou354@gmail.com>
2026-01-05 03:54:34 +0900

    [add]アイドルステートを追加

---
+++ Assets/ActionSample/Weapon/StateMachine/WeaponIdleState.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace ActionSample.Weapon.StateMachine
+{
+    public class WeaponIdleState : WeaponState
+    {
+        public WeaponIdleState(WeaponController context) : base(context) { }
+
+        public override void Enter()
+        {
+            base.Enter();
+        }
+
+        public override void LogicUpdate()
+        {
+            base.LogicUpdate();
+
+            // Reload Input
+            if (ctx.InputHandler.ReloadInput)
+            {
+                if (ctx.CurrentAmmo < ctx.MaxAmmo)
+                {
+                    ctx.StateMachine.ChangeState(ctx.ReloadState);
+                    return;
+                }
+            }
+
+            // Fire Input
+            if (ctx.InputHandler.FireInput)
+            {
+                if (ctx.CurrentAmmo > 0)
+                {
+                    ctx.StateMachine.ChangeState(ctx.FireState);
+                }
+                else
+                {
+                    // Empty Click sound?
+                    // Auto Reload?
+                    ctx.StateMachine.ChangeState(ctx.ReloadState);
+                }
+            }
+        }
+    }
+}
