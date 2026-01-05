c8c15cd45c66b4504e1dda5375b1ea3bd8ff315d
kenkatou354 <kenkatou354@gmail.com>
2026-01-05 22:22:03 +0900

    [add]idle中はプレイヤーの前方向にスライド方向にする

--- Assets/ActionSample/StateMachine/PlayerSlideState.cs
+++ Assets/ActionSample/StateMachine/PlayerSlideState.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace ActionSample.StateMachine
 {
@@ -13,12 +13,12 @@ namespace ActionSample.StateMachine
             base.Enter();
             slideTimer = ctx.SlideDuration;
 
-            // スライディング開始 (入力方向 or 前方)
-            Vector3 slideDir = ctx.InputHandler.MovementInput.magnitude > 0.1f 
-                ? ctx.InputHandler.MovementInput 
-                : ctx.transform.forward;
+            // スライディング開始 (入力方向 or 前方) - 入力をローカルからワールドへ変換
+            Vector3 worldInput = ctx.InputHandler.MovementInput.magnitude > 0.1f 
+                 ? ctx.transform.TransformDirection(ctx.InputHandler.MovementInput)
+                 : ctx.transform.forward;
 
-            ctx.Rigidbody.linearVelocity = slideDir * ctx.SlideSpeed;
+            ctx.Rigidbody.linearVelocity = worldInput * ctx.SlideSpeed;
             ctx.Rigidbody.linearDamping = ctx.SlideDrag;
         }
 
