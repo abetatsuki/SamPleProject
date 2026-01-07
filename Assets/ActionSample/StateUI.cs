e69064048af58e6dfcb3bf565aa75abd58e98cc6
kenkatou354 <kenkatou354@gmail.com>
2026-01-07 16:28:51 +0900

    [add]ステート状態をテキストに表示する

---
+++ Assets/ActionSample/StateUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using TMPro;
+namespace ActionSample
+{
+  public class StateUI : MonoBehaviour
+  {
+        [SerializeField] private PlayerController playerController;
+        private TMP_Text text;
+        private void Awake()
+        {
+            text = GetComponent<TMP_Text>();
+        }
+        private void Update()
+        {
+            text.text = playerController.StateMachine.CurrentState.GetType().Name;
+        }
+
+    }
+}
